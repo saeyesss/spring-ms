c73530438af68605611badc5ddf1acc1453182d2
saeyesss <94211639+saeyesss@users.noreply.github.com>
2025-07-14 13:01:40 +0530

    add product test

--- product/src/test/java/com/springms/product/ProductApplicationTests.java
+++ product/src/test/java/com/springms/product/ProductApplicationTests.java
@@ -1,15 +1,55 @@
 package com.springms.product;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApplicationTests {
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
 
-	@Test
-	void contextLoads() {
-	}
+    static {
+        mongoDBContainer.start();
+    }
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                  "name": "iphone 2",
+                  "description": "best one yet again",
+                  "price": 1999
+                }
+                """;
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("iphone 2"))
+                .body("description", Matchers.equalTo("best one yet again"))
+                .body("price", Matchers.equalTo(1999));
+
+    }
 
 }
